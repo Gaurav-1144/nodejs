87e44fe07707307e7baad5094587576465286c62
Gaurav singh nagar koti <gaurav.singheng598@gmail.com>
2022-11-16 17:47:15 +0530

    basic api creation

---
+++ BasicApiCreation/data.js
@@ -0,0 +1,17 @@
+ const data = [
+    {
+    Name:"Gaurav Singh Nagar koti",
+    Mobile:"9044865771",
+    Post:"Software Engineer",
+    Company:"Webkul"
+    },
+    {
+        Name:"Kamalesh Singh",
+        Mobile:"9616588543",
+        Post:"NIC",
+        Company:"NIC"
+    }
+];
+
+const userData =data;
+module.exports = userData;
